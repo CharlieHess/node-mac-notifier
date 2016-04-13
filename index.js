d20079657b22619d447d28de42c80eb225503236
Charlie Hess <charlie@slack-corp.com>
2016-04-12 18:14:01 -0700

    Export a Notification class that wraps the native module and adds EventTarget superpowers.

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-var Notification = require('bindings')('Notification');
-module.exports = Notification.MacNotification;
+"use strict";
+
+const EventTarget = require('event-target-shim');
+const MacNotification = require('bindings')('Notification').MacNotification
+
+module.exports = class Notification extends EventTarget {
+
+  constructor(title, options) {
+    super();
+    
+    if (!title) throw new Error('Title is required');
+    
+    options = options || {};
+    options.body = options.body || '';
+    
+    Object.assign(this, {title}, options);
+    
+    options.canReply = !!options.canReply;
+
+    let activated = (isReply, response) => {
+      if (isReply) {
+        this.dispatchEvent('reply', {target: this, response});
+      } else {
+        this.dispatchEvent('click', {target: this});
+      }
+    };
+
+    let args = Object.assign({title, activated}, options);
+    new MacNotification(args);
+  }
+
+  close() {
+  }
+};
