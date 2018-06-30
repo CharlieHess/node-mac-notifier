872f16123ac40036f061aaff7b73f915f498dc50
William Gulian <williamgulian@gmail.com>
2018-06-30 17:45:18 -0400

    Replace deprecated calls with the new variants for Node 10

--- mac_notification.mm
+++ mac_notification.mm
@@ -104,7 +104,7 @@ NAN_METHOD(MacNotification::New) {
     const int argc = 1;
     Local<Value> argv[argc] = {info[0]};
     Local<Function> cons = Nan::New(constructor);
-    info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+    info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
   }
 }
 
